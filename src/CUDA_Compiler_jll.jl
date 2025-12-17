4a816a2775fbfbbcbe177cd6fdb92400ef06d35a
jlbuild <juliabuildbot@gmail.com>
2025-12-17 07:02:23 +0000

    CUDA_Compiler_jll build 0.3.0+1

--- src/CUDA_Compiler_jll.jl
+++ src/CUDA_Compiler_jll.jl
@@ -8,5 +8,5 @@ Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("CUDA_Compiler")
-JLLWrappers.@generate_main_file("CUDA_Compiler", UUID("d1e2174e-dfdc-576e-b43e-73b79eb1aca8"))
+JLLWrappers.@generate_main_file("CUDA_Compiler", Base.UUID("d1e2174e-dfdc-576e-b43e-73b79eb1aca8"))
 end  # module CUDA_Compiler_jll
